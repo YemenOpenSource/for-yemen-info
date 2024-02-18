9c5bc158835a5e5fdbae216e084be0fbdf366663
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-02-19 01:09:19 +0300

    first try json2csv

--- app/Http/Controllers/Json2Csv.php
+++ app/Http/Controllers/Json2Csv.php
@@ -12,59 +12,95 @@ class Json2Csv extends Controller
 {
     public function __invoke(Request $request)
     {
-        $list = collect([
-            [
-                "governorate_name_en" => "Amant Al-Asmah",
-                "governorate_name_ar" => "أمانة العاصمة",
-                "governorate_name_ar_tashkeel" => "أَمانَة العاصِمَةِ",
-                "governorate_phone_numbering_plan" => "01",
-                "governorate_capital_name_ar" => "",
-                "governorate_capital_name_en" => "",
-                "governorate_name_ar_normalized" => "امانه العاصمه",
-                "governorate_name_en_normalized" => "Amant Al Asmah",
-
-                "district_name_en" => "Sana'a Al-qdimah",
-                "district_name_ar" => "صنعاء القديمة",
-                "district_name_ar_tashkeel" => "صَنْعاء القَدِيمَة",
-                "district_name_ar_normalized" => "صنعاء القديمه",
-                "district_name_en_normalized" => "Sanaa Al qdimah",
-
-                "uzlah_name_en" => "Old City",
-                "uzlah_name_ar" => "صنعاء القديمه",
-                "uzlah_name_ar_normalized" => "صنعاء القديمه",
-                "uzlah_name_en_normalized" => "Old City",
-
-                "village_name_en" => "Al-Amanh",
-                "village_name_ar" => "الامانة",
-                "village_name_ar_normalized" => "الامانه",
-                "village_name_en_normalized" => "Al Amanh"
-            ],
-        ]);
-
-        // Export all users
-        (new FastExcel($list))->export('file.csv');
-
-        return;
-
-        $json = Storage::get("public/yemen-info.json");
+        $json = Storage::get("public/yemen-info-small.json");
         $yemenData = json_decode($json, true);
-        $counter = 1;
+
+        $data = [];
+        $items = [];
 
         foreach ($yemenData['governorates'] as $gov) {
-            foreach ($gov['districts'] as $i => &$district) {
+            $data[] = [
+                "governorate_name_en" => $gov["name_en"],
+                "governorate_name_ar" => $gov["name_ar"],
+                "governorate_name_ar_tashkeel" => $gov["name_ar_tashkeel"],
+                "governorate_phone_numbering_plan" => $gov["phone_numbering_plan"],
+                "governorate_capital_name_ar" => $gov["capital_name_ar"],
+                "governorate_capital_name_en" => $gov["capital_name_en"],
+                "governorate_name_ar_normalized" => $gov["name_ar_normalized"],
+                "governorate_name_en_normalized" => $gov["name_en_normalized"],
+            ];
+
+            foreach ($gov['districts'] as $district) {
+                $data[] = [
+                    "district_name_en" => $district['name_en'],
+                    "district_name_ar" => $district['name_ar'],
+                    "district_name_ar_tashkeel" => $district['name_ar_tashkeel'],
+                    "district_name_ar_normalized" => $district['name_ar_normalized'],
+                    "district_name_en_normalized" => $district['name_en_normalized'],
+                ];
+                // dd($data);
                 if (isset($district['uzaal']) && is_array($district['uzaal'])) {
                     foreach ($district['uzaal'] as $uzlah) {
+                        $data[] = [
+                            "uzlah_name_en" => $uzlah["name_en"],
+                            "uzlah_name_ar" => $uzlah["name_ar"],
+                            "uzlah_name_ar_normalized" => $uzlah["name_ar_normalized"],
+                            "uzlah_name_en_normalized" => $uzlah["name_en_normalized"],
+                        ];
                         if (isset($uzlah['villages']) && is_array($uzlah['villages'])) {
                             foreach ($uzlah['villages'] as $village) {
-                                echo $village['name_ar_normalized'] . "<br />";
-                                $counter++;
-                                dd('s');
+                                $data[] = [
+                                    "village_name_en" => $village["name_en"],
+                                    "village_name_ar" => $village["name_ar"],
+                                    "village_name_ar_normalized" => $village["name_ar_normalized"],
+                                    "village_name_en_normalized" => $village["name_en_normalized"],
+                                ];
                             }
+                            // dd($data,$this->flattenArray($data));
+                            // $items[] = $this->flattenArray($data);
+                            $items[] = $data;
+                            // dd($items);
+                        } else {
+                            $data[] = [
+                                "village_name_en" => "",
+                                "village_name_ar" => "",
+                                "village_name_ar_normalized" => "",
+                                "village_name_en_normalized" => "",
+                            ];
+                            // $items[] = $this->flattenArray($data);
+                            $items[] = $data; // Add the uzlah data without village to the items array
+
                         }
                     }
+                } else {
+                    $data[] = [
+                        "uzlah_name_en" => "",
+                        "uzlah_name_ar" => "",
+                        "uzlah_name_ar_normalized" => "",
+                        "uzlah_name_en_normalized" => "",
+                        "village_name_en" => "",
+                        "village_name_ar" => "",
+                        "village_name_ar_normalized" => "",
+                        "village_name_en_normalized" => "",
+                    ];
+
+                    $items[] = $this->flattenArray($data);
                 }
             }
         }
-        // return $yemenData;
+
+        dd($items);
+
+        // (new FastExcel($list))->export('file.csv');
+    }
+
+
+    private function flattenArray($array)
+    {
+        $flattenedArray = [];
+        foreach ($array as $item) {
+            $flattenedArray = array_merge($flattenedArray, $item);
+        }
+        return $flattenedArray;
     }
 }

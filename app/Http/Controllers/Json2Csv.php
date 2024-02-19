5a64ba4e747a4a52d8a8229f3f7fbde7a3a904be
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2024-02-19 23:23:45 +0300

    json2csv is working fine, but the issue in the huge yemen-info.json file

--- app/Http/Controllers/Json2Csv.php
+++ app/Http/Controllers/Json2Csv.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 use Rap2hpoutre\FastExcel\FastExcel;
 
@@ -16,84 +17,112 @@ class Json2Csv extends Controller
         $yemenData = json_decode($json, true);
 
         $data = [];
-        $items = [];
+
+        $previousValues = $this->initializePreviousValues();
 
         foreach ($yemenData['governorates'] as $gov) {
-            $data[] = [
-                "governorate_name_en" => $gov["name_en"],
-                "governorate_name_ar" => $gov["name_ar"],
-                "governorate_name_ar_tashkeel" => $gov["name_ar_tashkeel"],
-                "governorate_phone_numbering_plan" => $gov["phone_numbering_plan"],
-                "governorate_capital_name_ar" => $gov["capital_name_ar"],
-                "governorate_capital_name_en" => $gov["capital_name_en"],
-                "governorate_name_ar_normalized" => $gov["name_ar_normalized"],
-                "governorate_name_en_normalized" => $gov["name_en_normalized"],
-            ];
+            $previousValues['governorate'] = $this->fillPreviousValues($gov, $previousValues['governorate'], 'governorate');
 
-            foreach ($gov['districts'] as $district) {
-                $data[] = [
-                    "district_name_en" => $district['name_en'],
-                    "district_name_ar" => $district['name_ar'],
-                    "district_name_ar_tashkeel" => $district['name_ar_tashkeel'],
-                    "district_name_ar_normalized" => $district['name_ar_normalized'],
-                    "district_name_en_normalized" => $district['name_en_normalized'],
-                ];
-                // dd($data);
+            foreach ($gov['districts'] as $i => $district) {
+                $previousValues['district'] = $this->fillPreviousValues($district, $previousValues['district'], 'district');
+                $districtData = $this->fillKeys($district, 'district', $previousValues);
+
                 if (isset($district['uzaal']) && is_array($district['uzaal'])) {
                     foreach ($district['uzaal'] as $uzlah) {
-                        $data[] = [
-                            "uzlah_name_en" => $uzlah["name_en"],
-                            "uzlah_name_ar" => $uzlah["name_ar"],
-                            "uzlah_name_ar_normalized" => $uzlah["name_ar_normalized"],
-                            "uzlah_name_en_normalized" => $uzlah["name_en_normalized"],
-                        ];
+                        $previousValues['uzlah'] = $this->fillPreviousValues($uzlah, $previousValues['uzlah'], 'uzlah');
+                        $uzlahData = $this->fillKeys($uzlah, 'uzlah', $previousValues);
+
                         if (isset($uzlah['villages']) && is_array($uzlah['villages'])) {
                             foreach ($uzlah['villages'] as $village) {
-                                $data[] = [
-                                    "village_name_en" => $village["name_en"],
-                                    "village_name_ar" => $village["name_ar"],
-                                    "village_name_ar_normalized" => $village["name_ar_normalized"],
-                                    "village_name_en_normalized" => $village["name_en_normalized"],
-                                ];
+                                $villageData = $this->fillKeys($village, 'village', $previousValues);
+                                $data[] = $villageData;
                             }
-                            // dd($data,$this->flattenArray($data));
-                            // $items[] = $this->flattenArray($data);
-                            $items[] = $data;
-                            // dd($items);
                         } else {
-                            $data[] = [
-                                "village_name_en" => "",
-                                "village_name_ar" => "",
-                                "village_name_ar_normalized" => "",
-                                "village_name_en_normalized" => "",
-                            ];
-                            // $items[] = $this->flattenArray($data);
-                            $items[] = $data; // Add the uzlah data without village to the items array
-
+                            // No villages under this uzlah, add empty village data
+                            $villageData = $this->fillKeys([], 'village', $previousValues);
+                            $data[] = $villageData;
                         }
                     }
                 } else {
-                    $data[] = [
-                        "uzlah_name_en" => "",
-                        "uzlah_name_ar" => "",
-                        "uzlah_name_ar_normalized" => "",
-                        "uzlah_name_en_normalized" => "",
-                        "village_name_en" => "",
-                        "village_name_ar" => "",
-                        "village_name_ar_normalized" => "",
-                        "village_name_en_normalized" => "",
+                    $previousValues['uzlah'] = [
+                        "name_en" => "",
+                        "name_ar" => "",
+                        "name_ar_normalized" => "",
+                        "name_en_normalized" => "",
                     ];
-
-                    $items[] = $this->flattenArray($data);
+                    $uzlahData = $this->fillKeys([], 'uzlah', $previousValues);
+                    $data[] = $uzlahData;
+                    $villageData = $this->fillKeys([], 'village', $previousValues);
+                    $data[] = $villageData;
                 }
             }
         }
 
-        dd($items);
+        $uniqueData = array_map("unserialize", array_unique(array_map("serialize", $data)));
 
-        // (new FastExcel($list))->export('file.csv');
+        (new FastExcel($uniqueData))->export('file.csv');
+        (new FastExcel($uniqueData))->export('file.xlsx');
     }
 
+
+    private function fillKeys($item, $type, &$previousValues)
+    {
+        $template = [
+            "governorate_name_en" => $previousValues['governorate']['name_en'] ?? "",
+            "governorate_name_ar" => $previousValues['governorate']['name_ar'] ?? "",
+            "governorate_name_ar_tashkeel" => $previousValues['governorate']['name_ar_tashkeel'] ?? "",
+            "governorate_phone_numbering_plan" => $previousValues['governorate']['phone_numbering_plan'] ?? "",
+            "governorate_capital_name_ar" => $previousValues['governorate']['capital_name_ar'] ?? "",
+            "governorate_capital_name_en" => $previousValues['governorate']['capital_name_en'] ?? "",
+            "governorate_name_ar_normalized" => $previousValues['governorate']['name_ar_normalized'] ?? "",
+            "governorate_name_en_normalized" => $previousValues['governorate']['name_en_normalized'] ?? "",
+            "district_name_en" => $previousValues['district']['name_en'] ?? "",
+            "district_name_ar" => $previousValues['district']['name_ar'] ?? "",
+            "district_name_ar_tashkeel" => $previousValues['district']['name_ar_tashkeel'] ?? "",
+            "district_name_ar_normalized" => $previousValues['district']['name_ar_normalized'] ?? "",
+            "district_name_en_normalized" => $previousValues['district']['name_en_normalized'] ?? "",
+            "uzlah_name_en" => $previousValues['uzlah']['name_en'] ?? "",
+            "uzlah_name_ar" => $previousValues['uzlah']['name_ar'] ?? "",
+            "uzlah_name_ar_normalized" => $previousValues['uzlah']['name_ar_normalized'] ?? "",
+            "uzlah_name_en_normalized" => $previousValues['uzlah']['name_en_normalized'] ?? "",
+            "village_name_en" => $item['name_en'] ?? "",
+            "village_name_ar" => $item['name_ar'] ?? "",
+            "village_name_ar_normalized" => $item['name_ar_normalized'] ?? "",
+            "village_name_en_normalized" => $item['name_en_normalized'] ?? ""
+        ];
+
+        $prefix = '';
+        if ($type !== 'governorate') {
+            $prefix = $type . '_';
+        }
+
+        $previousValues[$type] = $item;
+
+        return $template;
+    }
+
+    private function fillPreviousValues($item, $previousValues)
+    {
+        $filledValues = [];
+
+        foreach ($item as $key => $value) {
+            if (!empty($value)) {
+                $filledValues[$key] = $value;
+            }
+        }
+
+        return $filledValues + $previousValues;
+    }
+
+
+    private function initializePreviousValues()
+    {
+        return [
+            'governorate' => [],
+            'district' => [],
+            'uzlah' => []
+        ];
+    }
 
     private function flattenArray($array)
     {
